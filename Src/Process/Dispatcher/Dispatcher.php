61dde51dff623b72acd2751a192667a4c6f06861
flytachi <jasur.rakhmatov03@gmail.com>
2023-10-10 22:25:02 +0500

    Fix Bag

--- Src/Process/Dispatcher/Dispatcher.php
+++ Src/Process/Dispatcher/Dispatcher.php
@@ -2,6 +2,8 @@
 
 namespace Extra\Src\Process\Dispatcher;
 
+use Extra\Src\Log\Log;
+
 /**
  * Dispatcher
  *
@@ -22,10 +24,13 @@ abstract class Dispatcher
             if ($data) {
                 $fileName = uniqid("cast-cache-");
                 $filePath = PATH_CACHE . '/' . $fileName;
-                file_put_contents($filePath, serialize($data));
+                $serializeData = serialize($data);
+                file_put_contents($filePath, $serializeData);
                 chmod($filePath, 0777);
+                Log::trace(static::class . ' serialized => ' . $serializeData);
             }
 
+            Log::trace(static::class . ' DISPATCH');
             return exec(sprintf(
                 "php ../extra process run --class-name='%s' %s > %s 2>&1 & echo $!",
                 static::class,
