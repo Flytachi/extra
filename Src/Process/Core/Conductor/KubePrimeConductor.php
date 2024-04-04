8d11d8fd8e22cf06c2746aab37131086745cbe96
itachi <jaska.rakhmatov03@gmail.com>
2024-04-04 20:28:43 +0500

    Add file extension verification for CSV, JSON, XML read/write operations
    Updated the CSV, JSON, and XML abstract classes to add file extension verification in the read and write functions. This change now ensures that the file has the correct extension before attempting to read/write data, subsequently reducing potential read/write errors. Version numbers for respective classes were also updated to reflect these changes.

--- Src/Process/Core/Conductor/KubePrimeConductor.php
+++ Src/Process/Core/Conductor/KubePrimeConductor.php
@@ -2,14 +2,13 @@
 
 namespace Extra\Src\Process\Core\Conductor;
 
-use Extra\Src\Process\Kube\Prime\KubePrime;
 use Extra\Src\Sheath\File\JSON;
 
 class KubePrimeConductor implements Conductor
 {
     public function recordAdd(string $className, int $pid): void
     {
-         JSON::write(($className)::$STM_PATH ?? base64_encode($className),
+        JSON::write(($className)::$STM_PATH ?? base64_encode($className),
             [
                 'pid' => $pid,
                 'title' => $className,
