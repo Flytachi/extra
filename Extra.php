429a846ae83aca166231a42cc326eb4e189bf180
itachi <jaska.rakhmatov03@gmail.com>
2024-07-18 15:07:22 +0500

    Remove socket error handling and set up WebSocket server
    Removed obsolete classes `SocketError` and `WebServer` from the application, which related to socket error handling and server setup. Added new WebSocket server functionality in `Resource`, `Msg`, `WebSocketServerHandler`, and `WebSocketServer` classes. Handled process interruption, termination, listening to connections/messages, and more. Started applying a request per second (rps) limit for connections to prevent server overflow.

--- Extra.php
+++ Extra.php
@@ -19,7 +19,7 @@ use Extra\Src\HttpCode;
  * - `warningHandler($severity, $message, $file, $line): void`: Error handler for managing PHP warnings.
  * - `loadFunction(): void`: Loads all available functions from the Function directory.
  *
- * @version 4.5
+ * @version 4.6
  * @author Flytachi
  */
 class Extra
